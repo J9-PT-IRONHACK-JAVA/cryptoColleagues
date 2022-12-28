c3972b0a9793b4a1eedba21e2f182097c650fdbe
Carlos de Miguel Ferrero <cmiguel@plainconcepts.com>
2022-12-28 16:30:22 +0100

    Add Initial Backend + user management

---
+++ src/main/java/com/cryptocolleagues/dtos/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.cryptocolleagues.dtos;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+}
---
+++ src/test/java/com/cryptocolleagues/CryptoColleaguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptocolleagues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoColleaguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
